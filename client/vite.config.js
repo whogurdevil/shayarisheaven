84d3b6c53a5c34dd6672b41f5b993be529e4804d
whogurdevil <gur13dev@gmail.com>
2024-05-31 00:31:12 +0530

    production ready with volume and tested with mongoexport and import

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- base: "/",
- plugins: [react()],
- preview: {
-  port: 8080,
-  strictPort: true,
- },
- server: {
-  port: 8080,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:8080",
- },
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy options
+      "/shayari-website/assets": {
+        target: "http://127.0.0.1:5500/", // Your server URL
+        changeOrigin: true,
+        secure: false, // if you're using HTTPS, set it to true
+        headers: {
+          "Access-Control-Allow-Origin": "*", // or specific origin
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers":
+            "X-Requested-With, content-type, Authorization",
+        },
+      },
+    },
+  },
 });
